2cbb0cdb2c9392b8cf11a6d628dabfa2ab36d930
Nino Schelcher <onin1998@gmail.com>
2020-05-09 10:26:05 +0200

    back-end

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("public/index.html", (req, res) => res.send("Hello World!"));
 app.use(express.static("public"));
 
 app.use(function (req, res, next) {
