73aed7c304cde0e1103d4a59b1e09bb6fa9aaeb4
Nino Schelcher <onin1998@gmail.com>
2020-05-09 11:00:18 +0200

    routing

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("public/index.html", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/about", (req, res) => res.send("This is a about page"));
+app.get("/contact", (req, res) => res.send("This is a contact page"));
+
 app.use(express.static("public"));
 
 app.use(function (req, res, next) {
-  res.status(404).send("404 page not found");
+  res.status(404).send("404 Page not found");
 });
 
 app.listen(port, () =>
