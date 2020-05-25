c8597e4634a7ed435c36511da77c7f83ef50cf72
Nino Schelcher <onin1998@gmail.com>
2020-05-25 15:54:47 +0200

    linter and other stuff

--- server.js
+++ server.js
@@ -1,70 +1,73 @@
+/* eslint-disable import/newline-after-import */
+/* eslint-disable no-use-before-define */
 /* Express Setup */
-const express = require("express");
-const bodyParser = require("body-parser");
-const slug = require("slug");
-const find = require("array-find");
+const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const find = require('array-find');
 
-let users = [
+
+const users = [
   {
-    firstname: "test",
-    lastname: "test",
-    age: "test",
-    gender: "male",
-    lookingfor: "female",
+    firstname: 'test',
+    lastname: 'test',
+    age: 'test',
+    gender: 'male',
+    lookingfor: 'female',
   },
   {
-    firstname: "test2",
-    lastname: "test2",
-    age: "test2",
-    gender: "female",
-    lookingfor: "male",
+    firstname: 'test2',
+    lastname: 'test2',
+    age: 'test2',
+    gender: 'female',
+    lookingfor: 'male',
   },
 ];
 
 const answers = [
-  "Never, im lazy",
-  "Sometimes",
-  "2 times a week",
-  "Every single day!",
+  'Never, im lazy',
+  'Sometimes',
+  '2 times a week',
+  'Every single day!',
 ];
 
 /* Express chain routes and other stuff */
 app
-  .use(express.static(__dirname + "/public"))
+  .use(express.static(`${__dirname}/public`))
   .use(bodyParser.urlencoded({ extended: true }))
-  .get("/:firstname", getUser)
-  .set("view engine", "ejs")
-  .get("/steps", steps)
-  .get("/introduction", introduction)
-  .post("/", submit)
-  .use(function (req, res, next) {
-    res.status(404).send("404 Page not found");
+  .get('/:firstname', getUser)
+  .set('view engine', 'ejs')
+  .get('/steps', steps)
+  .get('/introduction', introduction)
+  .post('/', submit)
+  .use((req, res) => {
+    res.status(404).send('404 Page not found');
   })
   .listen(port);
 
-/* First ejs test for the process of telling more about your goals and interest 
-I have an array here with answers that dynamically create new answers in steps.ejs*/
+/* First ejs test for the process of telling
+more about your goals and interest */
 function steps(req, res) {
-  res.render("steps.ejs", {
-    page: "Step 2",
+  res.render('steps.ejs', {
+    page: 'Step 2',
     data: answers,
-    name: "Nino",
-    class: "steps",
+    name: 'Nino',
+    class: 'steps',
   });
 }
 
-//Render the introduction page
+// Render the introduction page
 function introduction(req, res) {
-  res.render("introduction.ejs", {
-    page: "Introduction",
-    class: "introduction",
+  res.render('introduction.ejs', {
+    page: 'Introduction',
+    class: 'introduction',
   });
 }
 
-//Function that picks up the filled in form and pushes all the data to the array,
-//after that you get redirected to the next step
+// Function that picks up the filled in form and pushes
+// all the data to the array,
+// after that you get redirected to the next step
 function submit(req, res) {
   users.push({
     firstname: req.body.firstname,
@@ -74,20 +77,18 @@ function submit(req, res) {
     lookingfor: req.body.preferredgender,
   });
 
-  res.redirect("/steps");
+  res.redirect('/steps');
 }
 
-//Function that renders a page with the specific
+// Function that renders a page with the specific
 function getUser(req, res, next) {
-  var firstname = req.params.firstname;
-  var user = find(users, function (value) {
-    return value.firstname === firstname;
-  });
+  const { firstname } = req.params;
+  const user = find(users, (value) => value.firstname === firstname);
 
   if (!user) {
     next();
     return;
   }
 
-  res.render("profile.ejs", { data: user, page: "Profile" });
+  res.render('profile.ejs', { data: user, page: 'Profile' });
 }
