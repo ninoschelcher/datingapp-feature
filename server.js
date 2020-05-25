f2a6dacdab4ef500140e9832012003a67c0b201e
Nino Schelcher <onin1998@gmail.com>
2020-05-25 23:09:04 +0200

    connected to database and other stuff

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable object-shorthand */
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/newline-after-import */
 /* eslint-disable no-use-before-define */
 /* Express Setup */
@@ -6,24 +10,21 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const find = require('array-find');
+const mongo = require('mongodb');
+const { ObjectID } = require('mongodb');
+require('dotenv').config();
 
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`;
 
-const users = [
-  {
-    firstname: 'test',
-    lastname: 'test',
-    age: 'test',
-    gender: 'male',
-    lookingfor: 'female',
-  },
-  {
-    firstname: 'test2',
-    lastname: 'test2',
-    age: 'test2',
-    gender: 'female',
-    lookingfor: 'male',
-  },
-];
+let db;
+mongo.MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log('Lets get this data started');
+  }
+  db = client.db(process.env.DB_NAME);
+});
 
 const answers = [
   'Never, im lazy',
@@ -36,11 +37,12 @@ const answers = [
 app
   .use(express.static(`${__dirname}/public`))
   .use(bodyParser.urlencoded({ extended: true }))
-  .get('/:firstname', getUser)
   .set('view engine', 'ejs')
   .get('/steps', steps)
   .get('/introduction', introduction)
-  .post('/', submit)
+  .get('/:id', getUserProfile)
+  .post('/', introductionForm)
+  .post('/updateuser', updateUserProfile)
   .use((req, res) => {
     res.status(404).send('404 Page not found');
   })
@@ -68,27 +70,60 @@ function introduction(req, res) {
 // Function that picks up the filled in form and pushes
 // all the data to the array,
 // after that you get redirected to the next step
-function submit(req, res) {
-  users.push({
+
+
+// Function that renders a page with the specific
+function getUserProfile(req, res, next) {
+  const { id } = req.params;
+
+  db.collection('users').findOne({
+    _id: new mongo.ObjectID(id),
+  }, showProfile);
+
+  function showProfile(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      res.render('profile.ejs', { data: data, page: 'Profile' });
+    }
+  }
+}
+
+function introductionForm(req, res, next) {
+  db.collection('users').insertOne({
     firstname: req.body.firstname,
     lastname: req.body.lastname,
     age: req.body.age,
     gender: req.body.gender,
     lookingfor: req.body.preferredgender,
-  });
+  }, profileRedirect);
 
-  res.redirect('/steps');
+  function profileRedirect(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      res.redirect(`/${data.insertedId}`);
+    }
+  }
 }
 
-// Function that renders a page with the specific
-function getUser(req, res, next) {
-  const { firstname } = req.params;
-  const user = find(users, (value) => value.firstname === firstname);
+function updateUserProfile(req, res, next) {
+  db.collection('users').updateOne({
+    _id: ObjectID(req.body._id),
+    $set: {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      age: req.body.age,
+      gender: req.body.gender,
+      lookingfor: req.body.preferredgender,
+    },
+  }, updatePage);
 
-  if (!user) {
-    next();
-    return;
+  function updatePage(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      res.redirect(`/${data.insertedId}`);
+    }
   }
-
-  res.render('profile.ejs', { data: user, page: 'Profile' });
 }
