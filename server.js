a4af486dae35b25765a69d1bec9e550a76fc1da7
Nino Schelcher <onin1998@gmail.com>
2020-05-28 18:16:21 +0200

    sessions and more database stuff

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const { ObjectID } = require('mongodb');
 const session = require('express-session');
 require('dotenv').config();
 
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`;
-
+const url = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST;
 let db;
+
 mongo.MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
   if (err) {
     throw err;
@@ -41,33 +41,32 @@ app
   .use(express.static(`${__dirname}/public`))
   .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
-  .use(session({ secret: '4nd912nd891nde82' }))
+  .use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_KEY,
+  }))
   .set('view engine', 'ejs')
   .post('/', introductionForm)
   .post('/step2form', submitStep2)
+  .post('/step3form', submitStep3)
+  .post('/step4form', submitStep4)
+  .post('/step5form', submitStep5)
   .post('/updateuser', updateUserProfile)
-  .get('/step2', loadStep2)
+  .get('/step2/:id', loadStep2)
+  .get('/step3/:id', loadStep3)
+  .get('/step4/:id', loadStep4)
+  .get('/step5/:id', loadStep5)
   .get('/', introduction)
-  .get('/:id', getUserProfile)
+  .get('/profile/:id', getUserProfile)
   .use((req, res) => {
     res.status(404).send('404 Page not found');
   })
   .listen(port);
 
-/* First ejs test for the process of telling
-more about your goals and interest */
-function loadStep2(req, res) {
-  res.render('step2.ejs', {
-    page: 'Step 2',
-    data: answers,
-    name: 'Nino',
-    class: 'steps',
-  });
-}
 
 // Render the introduction page
-function introduction(req, res, data) {
-  req.session.name = data._id;
+function introduction(req, res) {
   res.render('introduction.ejs', {
     page: 'Introduction',
     class: 'introduction',
@@ -75,50 +74,93 @@ function introduction(req, res, data) {
 }
 
 /* Function that sends the data filled in by the user to mongodb */
-function introductionForm(req, res, next) {
-  db.collection('users').insertOne({
+function introductionForm(req, res) {
+  req.session.user = {
+    id: req.body.username,
     firstname: req.body.firstname,
     lastname: req.body.lastname,
     age: req.body.age,
     gender: req.body.gender,
     preferredgender: req.body.preferredgender,
-  }, profileRedirect);
+  };
+  res.redirect('/step2/' + req.body.username);
+}
+
+
+/* First ejs test for the process of telling
+more about your goals and interest */
+function loadStep2(req, res) {
+  res.render('step2.ejs', {
+    page: 'Step 2',
+    user: req.session.user,
+  });
+}
+
+function submitStep2(req, res, next) {
+  req.session.user.dogname = req.body.dogname;
+  req.session.user.dogpicture = req.body.dogpicture;
+  res.redirect('/step3/' + req.session.user.id);
+}
+
+function loadStep3(req, res) {
+  res.render('step3.ejs', {
+    page: 'Step 3',
+    user: req.session.user,
+  });
+}
+
+function submitStep3(req, res, next) {
+  res.redirect('/step4/' + req.session.user.id);
+}
+
+function loadStep4(req, res) {
+  res.render('step4.ejs', {
+    page: 'Step 4',
+    user: req.session.user,
+  });
+}
+
+function submitStep4(req, res, next) {
+  res.redirect('/step5/' + req.session.user.id);
+}
+
+function loadStep5(req, res) {
+  res.render('step5.ejs', {
+    page: 'Step 5',
+    user: req.session.user,
+  });
+}
+
+function submitStep5(req, res, next) {
+  db.collection('users').insertOne(req.session.user, profileRedirect);
 
   function profileRedirect(err, data) {
     if (err) {
       next(err);
     } else {
-      console.log(data.insertedId);
-      res.redirect(`/${data.insertedId}`);
+      req.session.user._id = data.insertedId;
+      res.redirect('/profile/' + req.session.user._id);
     }
   }
 }
 
-function submitStep2(req, res, next) {
-  res.render('step3.ejs', { page: 'Step 3' });
-}
-
-
 // Function that renders a page with the specific profile of a specific user
 function getUserProfile(req, res, next) {
-  const { id } = req.params;
-
   db.collection('users').findOne({
-    _id: new mongo.ObjectID(id),
+    _id: new mongo.ObjectID(req.params),
   }, showProfile);
 
   function showProfile(err, data) {
     if (err) {
       next(err);
     } else {
-      res.render('profile.ejs', { data: data, page: 'Profile' });
+      res.render('profile.ejs', { user: data, page: 'Profile' });
     }
   }
 }
 
 /* Function that updates the fields from the profile page */
 function updateUserProfile(req, res, next) {
-  console.log(req.body._id);
   db.collection('users').updateOne({ _id: ObjectID(req.body._id) },
     {
       $set: {
@@ -130,11 +172,11 @@ function updateUserProfile(req, res, next) {
       },
     }, updatePage);
 
-  function updatePage(err, data) {
+  function updatePage(err) {
     if (err) {
       next(err);
     } else {
-      res.redirect(`/${req.body._id}`);
+      res.redirect('/' + req.body._id);
     }
   }
 }
