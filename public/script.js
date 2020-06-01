066313df85878ddf5520396a617f7aad7bb411bd
Nino Schelcher <onin1998@gmail.com>
2020-06-01 18:10:45 +0200

    profile enhancements

--- public/script.js
+++ public/script.js
@@ -1,18 +1,40 @@
 
 const step3form = document.querySelector('#step3form');
 const hobbiesinput = document.querySelectorAll('.hobbies');
-const firstHobby = document.querySelector('#fir')
+const firstHobby = document.querySelector('#hobby1');
 
 hobbiesinput.forEach(input => input.style.display = "none");
 
+if(firstHobby) {
+firstHobby.addEventListener('keyup', (event) => {
+   console.log(this.value);
+})};
 
 
-const loadDogPicture = (event) => {
+const profilePicture = document.querySelector('#profilepictures');
+const editProfilePicture = document.querySelector('#changeimage');
+profilePicture.addEventListener('mouseover', () => {
+    editProfilePicture.style.display = "block";
+})
+
+
+
+
+
+/* https://www.webtrickshome.com/faq/how-to-display-uploaded-image-in-html-using-javascript used preview images when uploading them */
+const dogPictureInput = document.querySelector('#dogpicture');
+const profilePictureInput = document.querySelector('#profilepicture');
+
+if(dogPictureInput) {
+dogPictureInput.addEventListener('change', (event) => {
     const dogPicture = document.getElementById('picture-dog');
     dogPicture.src = URL.createObjectURL(event.target.files[0]);
-};
+});
+}
 
-const loadProfilePicture = (event) => {
+if(profilePictureInput) {
+profilePictureInput.addEventListener('change', (event) => {
     const profilePicture = document.getElementById('picture-profile');
     profilePicture.src = URL.createObjectURL(event.target.files[0]);
+});
 }
